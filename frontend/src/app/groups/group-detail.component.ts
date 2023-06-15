043654a02dd04cb0c82186427f57ba7671d10bd9
koeda <yohannesteshle@gmail.com>
2023-06-15 10:44:03 -0500

    finalized the transaction and member addition

--- frontend/src/app/groups/group-detail.component.ts
+++ frontend/src/app/groups/group-detail.component.ts
@@ -2,90 +2,108 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Component, inject } from '@angular/core';
 import { GroupsService, IGroup, IMember } from './groups.service';
 import { StateService } from '../user/state.service';
+import {initDials, initTooltips} from "flowbite";
 
 @Component({
   selector: 'app-group-detail',
   template: `
-    <div class="mt-96" *ngIf="!group; else details">
-      <div role="status" class=" flex w-full h-full justify-center items-center">
-        <svg aria-hidden="true" class="inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-        </svg>
-        <span class="sr-only">Loading...</span>
+      <div class="mt-96" *ngIf="!group; else details">
+          <div role="status" class=" flex w-full h-full justify-center items-center">
+              <svg aria-hidden="true"
+                   class="inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                   viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"/>
+              </svg>
+              <span class="sr-only">Loading...</span>
+          </div>
       </div>
-    </div>
 
-    <app-header/>
-    <ng-template #details>
-    <div class="mt-20 max-w-4xl mx-auto">
+      <app-header/>
+      <ng-template #details>
+          <div class="mt-20 max-w-4xl mx-auto">
 
-      <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-          <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+              <section class="bg-white dark:bg-gray-900">
+                  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
+                      <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
 
-            <h1 class="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">{{group.title}}</h1>
-            <div class=" flex border border-gray-300 rounded-lg p-5 m-5 justify-around">
-                <div class="flex flex-col justify-center items-center">
-                  <p>Total Money Spent:</p>
-                  <p class="text-4xl font-bold text-red-500"> {{getSpend()| currency}}</p>
+                          <h1 class="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">{{group.title}}</h1>
+                          <div class=" flex border border-gray-300 rounded-lg p-5 m-5 justify-around">
+                              <div class="flex flex-col justify-center items-center">
+                                  <p>Total Money Spent:</p>
+                                  <p class="text-4xl font-bold text-red-500"> {{getSpend()| currency}}</p>
 
-                </div>
-              <div class="flex flex-col justify-center items-center">
-                <p>Total Money Spent by you:</p>
-                <p class="text-4xl font-bold text-green-500"> {{getSpendBy(stateService.user()._id)|currency}}</p>
+                              </div>
+                              <div class="flex flex-col justify-center items-center">
+                                  <p>Total Money Spent by you:</p>
+                                  <p class="text-4xl font-bold text-green-500"> {{getSpendBy(stateService.user()._id)|currency}}</p>
 
+                              </div>
+                              <div class="flex flex-col justify-center items-center">
+                                  <p>Total Money You Owe:</p>
+                                  <p [class]="colorizeOwe(getOwesBy(stateService.user()._id))">{{getOwesBy(stateService.user()._id)| currency}}</p>
+
+                              </div>
+
+                          </div>
+                          <a (click)="showMembers=true; showTrasactions=false"
+                             class=" mb-4 cursor-pointer inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                              show Members
+                              <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                                   xmlns="http://www.w3.org/2000/svg">
+                                  <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                              </svg>
+                          </a>
+
+                          <p class="text-lg font-normal  text-gray-500 dark:text-gray-400 line-clamp-2">Static websites
+                              are now used to bootstrap lots of websites and are becoming the basis for a variety of
+                              tools that even influence both web designers and developers.</p>
+
+                      </div>
+
+                      <!-- Modal toggle -->
+
+                      <div class="grid md:grid-cols-2 gap-8">
+
+
+                      </div>
+                  </div>
+              </section>
+          </div>
+          <div class="max-w-2xl mx-auto mb-10">
+
+              <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+                  <ul class="flex flex-wrap -mb-px justify-around">
+                      <li class="mr-2">
+                          <a (click)="showTrasactions=true; showMembers=false"
+                             class=" cursor-pointer inline-block p-4 hover:text-blue-600 active:border-b-2 border-blue-600 rounded-t-lg  active:text-blue-500 active:border-blue-500"
+                             aria-current="page">Transactions</a>
+                      </li>
+                      <li class="mr-2">
+                          <a (click)="showTrasactions=false; showMembers=true"
+                             class=" cursor-pointer inline-block p-4 hover:text-blue-600 active:border-b-2 border-blue-600 rounded-t-lg  active:text-blue-500 active:border-blue-500"
+                             aria-current="page">Members</a>
+                      </li>
+
+                  </ul>
               </div>
-              <div class="flex flex-col justify-center items-center">
-                <p>Total Money You Owe:</p>
-                <p [class]="colorizeOwe(getOwesBy(stateService.user()._id))">{{getOwesBy(stateService.user()._id)| currency}}</p>
-
-              </div>
-
-            </div>
-            <a (click)="showMembers=true; showTrasactions=false" class=" mb-4 cursor-pointer inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-              Add Members
-              <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-
-            <p class="text-lg font-normal  text-gray-500 dark:text-gray-400 line-clamp-2">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.</p>
 
           </div>
 
-          <!-- Modal toggle -->
-
-          <div class="grid md:grid-cols-2 gap-8">
-
-
+          <div class="w-full">
+              <app-transaction [transactions]="group.transactions" (remove)="deleteTransaction($event)"
+                               *ngIf="showTrasactions"/>
+              <app-members [members]="group.members" *ngIf="showMembers" (remove)="deleteMember($event)"/>
+              <div class="mt-10 max-w-2xl mx-auto flex justify-end">
+                <app-add-transaction *ngIf="!showMembers "/>
+               <app-invite-member *ngIf="showMembers"/>
+              </div>
           </div>
-        </div>
-      </section>
-    </div>
-    <div class="max-w-2xl mx-auto mb-10">
-
-      <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-        <ul class="flex flex-wrap -mb-px justify-around">
-          <li class="mr-2">
-            <a  (click)="showTrasactions=true; showMembers=false" class=" cursor-pointer inline-block p-4 hover:text-blue-600 active:border-b-2 border-blue-600 rounded-t-lg  active:text-blue-500 active:border-blue-500" aria-current="page">Transactions</a>
-          </li>
-          <li class="mr-2">
-            <a (click)="showTrasactions=false; showMembers=true" class=" cursor-pointer inline-block p-4 hover:text-blue-600 active:border-b-2 border-blue-600 rounded-t-lg  active:text-blue-500 active:border-blue-500" aria-current="page">Members</a>
-          </li>
-
-        </ul>
-      </div>
-
-    </div>
-
-      <div class="w-full">
-    <app-transaction [transactions]="group.transactions" (remove)="deleteTransaction($event)" *ngIf="showTrasactions" />
-        <app-members [members]="group.members" *ngIf="showMembers"/>
-    </div>
-    </ng-template>
-    <div class="max-w-3xl flex items-end justify-end">
-
-    <app-add-transaction />
-    </div>
+      </ng-template>
 
   `,
   styles: [
@@ -110,6 +128,9 @@ export class GroupDetailComponent {
       this.groupId = params.get('group_id') as string;
       this.loadGroupDetails();
     });
+
+    initTooltips();
+    initDials();
   }
 
   private loadGroupDetails() {
--- frontend/src/app/groups/invite-member.component.ts
+++ frontend/src/app/groups/invite-member.component.ts
@@ -2,45 +2,95 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { GroupsService } from './groups.service';
+import {Modal, ModalOptions} from "flowbite";
 
 @Component({
   selector: 'app-invite-member',
   template: `
-    <h2>Invite Member</h2>
+      <!--    <h2>Invite Member</h2>-->
 
-    <form
-      class="container"
-      [formGroup]="inviteForm"
-      (ngSubmit)="inviteMember()"
-    >
-      <input type="email" placeholder="Email" formControlName="email" />
-      <div *ngIf="email?.touched && email?.invalid">
-        <div [style.color]="'red'" *ngIf="email?.errors?.['required']">
-          Email is required.
-        </div>
-        <div [style.color]="'red'" *ngIf="email?.errors?.['email']">
-          Invalid email format.
-        </div>
+      <!--    <form-->
+      <!--      class="container"-->
+      <!--      [formGroup]="inviteForm"-->
+      <!--      (ngSubmit)="inviteMember()"-->
+      <!--    >-->
+      <!--      <input type="email" placeholder="Email" formControlName="email" />-->
+      <!--      <div *ngIf="email?.touched && email?.invalid">-->
+      <!--        <div [style.color]="'red'" *ngIf="email?.errors?.['required']">-->
+      <!--          Email is required.-->
+      <!--        </div>-->
+      <!--        <div [style.color]="'red'" *ngIf="email?.errors?.['email']">-->
+      <!--          Invalid email format.-->
+      <!--        </div>-->
+      <!--      </div>-->
+      <!--      <button type="submit" [disabled]="inviteForm.invalid">Invite</button>-->
+      <!--      &lt;!&ndash; Go Back Button &ndash;&gt;-->
+      <!--      <button (click)="goBack()">Back</button>-->
+      <!--    </form>-->
+      <div>
+
+          <!-- Modal toggle -->
+          <button (click)="showModal()" type="button" data-dial-toggle="speed-dial-menu-default"
+                  aria-controls="speed-dial-menu-default" aria-expanded="false"
+                  class="flex items-center justify-center text-white bg-blue-700 rounded-full w-14 h-14 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
+              <svg aria-hidden="true" class="w-8 h-8 transition-transform group-hover:rotate-45" fill="none"
+                   stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+              </svg>
+              <span class="sr-only">Open actions menu</span>
+          </button>
+
+
+          <!-- Main modal -->
+          <div id="custom-modal" tabindex="-1" aria-hidden="true"
+               class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+              <div class="relative w-full max-w-md max-h-full">
+                  <!-- Modal content -->
+                  <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                      <button type="button"
+                              class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                              data-modal-hide="custom-modal" id="close-icon">
+                          <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                               xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"></path>
+                          </svg>
+                          <span class="sr-only">Close modal</span>
+                      </button>
+                      <div class="px-6 py-6 lg:px-8">
+                          <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Invite a Group Member</h3>
+                          <form class="space-y-6" [formGroup]="inviteForm" (ngSubmit)="inviteMember()">
+                              <div>
+                                  <label for="email"
+                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">email</label>
+                                  <input type="email" name="email" id="email"
+                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                         placeholder="name@company.com" formControlName="email">
+                                  <div *ngIf="email?.touched && email?.invalid">-->
+                                      <div [style.color]="'red'" *ngIf="email?.errors?.['required']">
+                                          Email is required.
+                                      </div>
+                                      <div [style.color]="'red'" *ngIf="email?.errors?.['email']">
+                                          Invalid email format.
+                                      </div>
+                                  </div>
+                              </div>
+                              <button type="submit"
+                                      class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+                                      data-modal-hide="custom-modal">Submit
+                              </button>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+
       </div>
-      <button type="submit" [disabled]="inviteForm.invalid">Invite</button>
-      <!-- Go Back Button -->
-      <button (click)="goBack()">Back</button>
-    </form>
   `,
-  styles: [
-    `
-      .container {
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 10px;
-      }
-      div,
-      input,
-      button {
-        margin-bottom: 10px;
-      }
-    `,
-  ],
+  styles: [``,],
 })
 export class InviteMemberComponent {
   groupId!: string;
@@ -78,5 +128,36 @@ export class InviteMemberComponent {
 
   goBack() {
     this.router.navigate(['', 'groups', 'detail', this.groupId]);
+  }
+  showModal(){
+
+    const $modalElement = document.querySelector('#custom-modal') as HTMLElement;
+
+    const modalOptions = {
+      placement: 'center',
+      backdrop: 'dynamic',
+      backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+      onHide: () => {
+        console.log('modal is hidden');
+      },
+      onShow: () => {
+        console.log('modal is shown');
+      },
+      onToggle: () => {
+        console.log('modal has been toggled');
+      }
+    }
+
+    const modal = new Modal($modalElement, modalOptions as ModalOptions);
+    modal._init()
+    modal.show();
+
+    const icon = document.getElementById('close-icon');
+    if(icon)
+      icon.addEventListener('click', () => {
+        modal.hide();
+      })
+
+
   }
 }

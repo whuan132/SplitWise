13e6d14a2ec92a5d124935a73c6905420a0dfa17
Wenhong Huang <wenhong.huang@miu.edu>
2023-06-15 14:40:12 -0500

    fix invite modal errors

--- frontend/src/app/groups/list.component.ts
+++ frontend/src/app/groups/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { GroupsService, IGroup } from './groups.service';
 import { Router } from '@angular/router';
 import { StateService } from '../user/state.service';
@@ -6,66 +6,78 @@ import { StateService } from '../user/state.service';
 @Component({
   selector: 'app-list',
   template: `
-      <app-header/>
-      <div class="mt-20 max-w-4xl mx-auto w-full">
-        <h2 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">My Spending Groups</h2>
+    <app-header />
+    <div class="mt-20 max-w-4xl mx-auto w-full">
+      <h2
+        class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white"
+      >
+        My Spending Groups
+      </h2>
 
-
-        <h3 *ngIf="!groups; else list">Loading...</h3>
+      <h3 *ngIf="!groups; else list">Loading...</h3>
       <ng-template #list>
-          <div class="space-y-2" >
-              <div *ngFor="let g of groups" >
+        <div class="space-y-2">
+          <div *ngFor="let g of groups">
+            <div
+              class="flex flex-col  items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-2xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+            >
+              <a (click)="gotoDetail(g)" class="flex">
+                <!--                      <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"-->
+                <!--                           src="../../assets/images/logo.png" alt="">-->
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                  <h5
+                    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                  >
+                    {{ g.title }} - {{ g.members[0].fullname }}
+                  </h5>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    Here are the biggest enterprise technology acquisitions of
+                    2021 so far, in reverse chronological order.
+                  </p>
+                </div>
+              </a>
+              <div>
+                <button
+                  *ngIf="isPending(g)"
+                  (click)="acceptGroup(g)"
+                  class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                >
+                  Accept
+                </button>
+                <button
+                  *ngIf="isPending(g)"
+                  (click)="rejectGroup(g._id)"
+                  class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                >
+                  Reject
+                </button>
 
-
-                <div class="flex flex-col  items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-2xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-
-                  <a [routerLink]="['', 'groups', 'detail', g._id]"
-                     [routerLinkActive]="isPending(g) ? 'false' : 'true'"
-                     class="flex">
-<!--                      <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"-->
-<!--                           src="../../assets/images/logo.png" alt="">-->
-                      <div class="flex flex-col justify-between p-4 leading-normal">
-                          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {{ g.title }}
-                              - {{ g.members[0].fullname }}</h5>
-                          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
-                              technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                      </div>
-
-                  </a>
-                  <div>
-                  <button *ngIf="isPending(g)" (click)="acceptGroup(g)" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Accept</button>
-                  <button *ngIf="isPending(g)" (click)="rejectGroup(g._id)" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-                    Reject
-                  </button>
-
-                  <button *ngIf="!isPending(g)" (click)="deleteGroup(g._id)"  class=" bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 text-red-500">Remove</button>
-                  </div>
-                  </div>
-
-
-
-
+                <button
+                  *ngIf="!isPending(g)"
+                  (click)="deleteGroup(g._id)"
+                  class=" bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 text-red-500"
+                >
+                  Remove
+                </button>
               </div>
-            <div class="w-full mx-auto">
-<!--            <button type="button" class="focus:outline-none text-white bg-purple-700 max-w-2xl w-full hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" (click)="addGroup()">Add New Spending Group</button>-->
-
             </div>
-
           </div>
-
+          <div class="w-full mx-auto">
+            <!--            <button type="button" class="focus:outline-none text-white bg-purple-700 max-w-2xl w-full hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" (click)="addGroup()">Add New Spending Group</button>-->
+          </div>
+        </div>
       </ng-template>
-        <app-modal title="Create A new Group" [fields]="['title']" button="Create new Group" (add)="addGroup($event)"/>
-      </div>
-
-
+      <app-modal
+        title="Create A new Group"
+        [fields]="['title']"
+        button="Create new Group"
+        (add)="addGroup($event)"
+      />
+    </div>
   `,
-  styles: [
-    `
-
-    `,
-  ],
+  styles: [],
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
   groups!: IGroup[];
 
   private stateService = inject(StateService);
@@ -77,7 +89,7 @@ export class ListComponent {
       (res) => {
         if (res && res.success) {
           this.groups = res.data as IGroup[];
-          this.groups.sort((m1, m2) => (this.isPending(m1) ? 1 : -1));
+          this.groups.sort((m1, _) => (this.isPending(m1) ? 1 : -1));
         }
       },
       (error) => {
@@ -105,7 +117,7 @@ export class ListComponent {
           }
           return false;
         });
-        this.groups.sort((m1, m2) => (this.isPending(m1) ? 1 : -1));
+        this.groups.sort((m1, _) => (this.isPending(m1) ? 1 : -1));
       },
       (error) => {
         console.log(error);
@@ -116,7 +128,9 @@ export class ListComponent {
   rejectGroup(group_id: string) {
     this.groupService.rejectGroup(group_id).subscribe(
       (res) => {
-        this.groups = this.groups.filter((g) => g._id !== group_id);
+        if (res && res.success) {
+          this.groups = this.groups.filter((g) => g._id !== group_id);
+        }
       },
       (error) => {
         console.log(error);
@@ -127,7 +141,9 @@ export class ListComponent {
   deleteGroup(group_id: string) {
     this.groupService.deleteGroup(group_id).subscribe(
       (res) => {
-        this.groups = this.groups.filter((g) => g._id !== group_id);
+        if (res && res.success) {
+          this.groups = this.groups.filter((g) => g._id !== group_id);
+        }
       },
       (error) => {
         console.log(error);
@@ -135,14 +151,17 @@ export class ListComponent {
     );
   }
 
-  addGroup(title : string) {
-    console.log(typeof title)
-
-      this.groupService.addGroup({title}).subscribe((res) => {
-        console.log(res)
-       this.groups.unshift(res.data)
-
-      });
+  addGroup(title: string) {
+    console.log(typeof title);
+    this.groupService.addGroup({ title }).subscribe((res) => {
+      console.log(res);
+      this.groups.unshift(res.data);
+    });
+  }
+  gotoDetail(group: IGroup) {
+    if (this.isPending(group)) {
+      return;
     }
-
+    this.router.navigate(['', 'groups', 'detail', group._id]);
+  }
 }
--- frontend/src/app/groups/modal.component.ts
+++ frontend/src/app/groups/modal.component.ts
@@ -1,86 +1,123 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
-import {Modal, ModalOptions} from "flowbite";
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Modal, ModalOptions } from 'flowbite';
 
 @Component({
   selector: 'app-modal',
   template: `
     <div>
-
       <!-- Modal toggle -->
-      <button (click)="showModal()" *ngIf="!button" type="button" data-dial-toggle="speed-dial-menu-default"
-      aria-controls="speed-dial-menu-default" aria-expanded="false"
-      class="flex items-center justify-center text-white bg-blue-700 rounded-full w-14 h-14 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
-      <svg aria-hidden="true" class="w-8 h-8 transition-transform group-hover:rotate-45" fill="none"
-           stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-      </svg>
-      <span class="sr-only">Open actions menu</span>
-      </button>
-      <button *ngIf="button" (click)="showModal()" class="block text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800 w-full max-w-2xl" type="button">
-        {{button}}
+      <button
+        *ngIf="button"
+        (click)="showModal()"
+        data-modal-target="custom-modal"
+        class="block text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800 w-full max-w-2xl"
+        type="button"
+      >
+        {{ button }}
       </button>
 
       <!-- Main modal -->
-      <div id="custom-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      <div
+        id="custom-modal"
+        tabindex="-1"
+        aria-hidden="true"
+        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      >
         <div class="relative w-full max-w-md max-h-full">
           <!-- Modal content -->
           <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="custom-modal" id="close-icon">
-              <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <button
+              (click)="hideModal()"
+              type="button"
+              class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+              id="close-icon"
+            >
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
               <span class="sr-only">Close modal</span>
             </button>
             <div class="px-6 py-6 lg:px-8">
-              <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">{{title}}</h3>
-              <form class="space-y-6" action="#" [formGroup]="form" (ngSubmit)="onSubmit()">
+              <h3
+                class="mb-4 text-xl font-medium text-gray-900 dark:text-white"
+              >
+                {{ title }}
+              </h3>
+              <form
+                class="space-y-6"
+                action="#"
+                [formGroup]="form"
+                (ngSubmit)="onSubmit()"
+              >
                 <div *ngFor="let field of fields">
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{field}}</label>
-                  <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com"  formControlName="field">
+                  <input
+                    type="text"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    [placeholder]="field"
+                    formControlName="field"
+                  />
                 </div>
-                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 " data-modal-hide="custom-modal" >Submit</button>
+                <button
+                  type="submit"
+                  class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+                  data-modal-hide="custom-modal"
+                >
+                  Submit
+                </button>
               </form>
             </div>
           </div>
         </div>
       </div>
-
-
     </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class ModalComponent {
-  closeModal='';
-  @Input({required: true, alias: 'title'}) title!:string;
-@Input({required: true, alias: 'fields'}) fields!: string[];
-@Input({required:false, alias:'button'}) button!:string;
-@Output('add') groupName= new EventEmitter<string>();
-form= inject(FormBuilder).group({
-  field:['',[Validators.required,Validators.min(5)]]
-})
-  onSubmit(){
-    console.log(this.form.value.field)
-    if(this.form.status==='VALID') {
+  closeModal = '';
+  @Input({ required: true, alias: 'title' }) title!: string;
+  @Input({ required: true, alias: 'fields' }) fields!: string[];
+  @Input({ required: false, alias: 'button' }) button!: string;
+  @Output('add') groupName = new EventEmitter<string>();
+  form = inject(FormBuilder).group({
+    field: ['', [Validators.required, Validators.min(5)]],
+  });
 
-      this.groupName.emit(this.form.value.field as string)
+  private modal!: Modal;
 
-
-    } else{
-      console.log("invalid form")
+  onSubmit() {
+    console.log(this.form.value.field);
+    if (this.form.status === 'VALID') {
+      if (this.modal) {
+        this.modal.hide();
+      }
+      this.groupName.emit(this.form.value.field as string);
+      this.form.reset();
+    } else {
+      console.log('invalid form');
     }
-
-
-}
-  showModal(){
-
-    const $modalElement = document.querySelector('#custom-modal') as HTMLElement;
+  }
+  showModal(): void {
+    const $modalElement = document.querySelector(
+      '#custom-modal'
+    ) as HTMLElement;
 
     const modalOptions = {
       placement: 'center',
       backdrop: 'dynamic',
-      backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+      backdropClasses:
+        'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
       onHide: () => {
         console.log('modal is hidden');
       },
@@ -89,21 +126,23 @@ form= inject(FormBuilder).group({
       },
       onToggle: () => {
         console.log('modal has been toggled');
-      }
-    }
+      },
+    };
 
-    const modal = new Modal($modalElement, modalOptions as ModalOptions);
-    modal._init()
-    modal.show();
+    this.modal = new Modal($modalElement, modalOptions as ModalOptions);
+    this.modal._init();
+    this.modal.show();
 
-      const icon = document.getElementById('close-icon');
-      if(icon)
-      icon.addEventListener('click', () => {
-        modal.hide();
-      })
-
-
+    // const icon = document.getElementById('close-icon');
+    // if (icon)
+    //   icon.addEventListener('click', () => {
+    //     this.modal.hide();
+    //   });
   }
 
+  hideModal() {
+    if (this.modal) {
+      this.modal.hide();
+    }
+  }
 }
-

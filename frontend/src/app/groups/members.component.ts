42a2d392b1ca68ce37596dd1c7ee8ad4db09937a
Wenhong Huang <wenhong.huang@miu.edu>
2023-06-16 22:17:32 -0500

    show pending tag on member item

--- frontend/src/app/groups/members.component.ts
+++ frontend/src/app/groups/members.component.ts
@@ -21,18 +21,24 @@ import { IGroupResult } from '../utils/GroupHelper';
       <div *ngFor="let user of group.members">
         <h2 id="{{ 'accordion-heading-' + user.user_id }}">
           <button
-            type="button"
             (click)="showMemberDetail('accordion-heading-' + user.user_id)"
             (dblclick)="closeMemberDetail('accordion-heading-' + user.user_id)"
+            type="button"
             class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border  border-gray-200 rounded-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
             aria-expanded="true"
             aria-controls="'accordion-example-body"
           >
-            <span class="space-x-2"
-              >{{ user.fullname | titlecase }}
-              <span class="mb-2 text-gray-500 dark:text-gray-400"></span>
-              {{ user.email }}</span
-            >
+            <div class="inline-flex flex-row">
+              <span class="space-x-2">
+                {{ user.fullname | titlecase }} {{ user.email }}
+              </span>
+              <span
+                *ngIf="user.pending"
+                class="bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300 ml-3 cursor-pointer"
+              >
+                Pending
+              </span>
+            </div>
             <button (click)="onRemove(user.email)" class="text-red text-sm">
               remove
             </button>
@@ -64,7 +70,9 @@ import { IGroupResult } from '../utils/GroupHelper';
               </div>
             </div>
             <div class="flex justify-end">
-              <a (click)="showMembers(user.user_id)" class="cursor-pointer inline-flex justify-center items-center py-2.5 px-5 text-sm text-blue-500  text-center  rounded-lg  hover:ring-blue-800 focus:ring-4 focus:ring-blue-300 "
+              <a
+                (click)="showMembers(user.user_id)"
+                class="cursor-pointer inline-flex justify-center items-center py-2.5 px-5 text-sm text-blue-500  text-center  rounded-lg  hover:ring-blue-800 focus:ring-4 focus:ring-blue-300 "
               >
                 show Transaction
                 <svg
@@ -93,13 +101,16 @@ export class MembersComponent implements AfterViewInit {
   @Input({ required: true, alias: 'group' }) group!: IGroup;
   @Input({ required: true, alias: 'groupResult' }) result!: IGroupResult;
   @Output('remove') removeMember = new EventEmitter<string>();
-  @Output('showMemberTransaction') showMemberTransaction = new EventEmitter<string>();
+  @Output('showMemberTransaction') showMemberTransaction =
+    new EventEmitter<string>();
+
   accordionItems!: AccordionItem[];
   members!: AccordionInterface;
 
   onRemove(email: string) {
     this.removeMember.emit(email);
   }
+
   ngAfterViewInit() {
     console.log(this.group);
     this.accordionItems = this.group.members.map((x) => {
@@ -135,10 +146,17 @@ export class MembersComponent implements AfterViewInit {
   }
 
   showMemberDetail(id: string) {
+    if (!this.result.members[id]) {
+      return;
+    }
     console.log(id);
     this.members.close(id);
   }
+
   closeMemberDetail(id: string) {
+    if (!this.result.members[id]) {
+      return;
+    }
     console.log(id);
     this.members.open(id);
   }
@@ -147,7 +165,8 @@ export class MembersComponent implements AfterViewInit {
     if (amount <= 0) return 'text-4xl font-bold text-green-500';
     else return 'text-4xl font-bold text-red-500';
   }
-  showMembers(id : string | undefined){
+
+  showMembers(id: string | undefined) {
     this.showMemberTransaction.emit(id as string);
   }
 }

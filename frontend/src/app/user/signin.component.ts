5a6d9c3a523d6532865cdf67c9faad0b2a0d7e66
Wenhong Huang <wenhong.huang@miu.edu>
2024-05-04 15:23:02 -0500

    ui

--- frontend/src/app/user/signin.component.ts
+++ frontend/src/app/user/signin.component.ts
@@ -4,62 +4,115 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IUser, StateService } from './state.service';
 import { Router } from '@angular/router';
 import { UserService } from './user.service';
-import {ToastService} from "../toast.service";
+import { ToastService } from '../toast.service';
 
 @Component({
   selector: 'app-signin',
   template: `
     <div>
       <section class="bg-gray-50 dark:bg-gray-900">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            <img class=" h-8 mr-2" src="../../assets/images/logo.png" alt="logo">
+        <div
+          class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+        >
+          <a
+            href="#"
+            class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img
+              class=" h-8 mr-2"
+              src="../../assets/images/logo.png"
+              alt="logo"
+            />
             CoinSplit
           </a>
           <div
-            class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+          >
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <h1
+                class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+              >
                 Sign in to your account
               </h1>
-              <form class="space-y-4 md:space-y-6" [formGroup]="signinForm" (ngSubmit)="signIn()">
+              <form
+                class="space-y-4 md:space-y-6"
+                [formGroup]="signinForm"
+                (ngSubmit)="signIn()"
+              >
                 <div>
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                    email</label>
-                  <input type="email" name="email" id="email"
-                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
-                         placeholder="name@company.com" required="true" formControlName="email">
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Your email</label
+                  >
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autocomplete="username"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
+                    placeholder="name@company.com"
+                    required="true"
+                    formControlName="email"
+                  />
                 </div>
                 <div>
-                  <label for="password"
-                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••"
-                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                         required="true" formControlName="password">
+                  <label
+                    for="password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Password</label
+                  >
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autocomplete="current-password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required="true"
+                    formControlName="password"
+                  />
                 </div>
                 <div class="flex items-center justify-between">
                   <div class="flex items-start">
                     <div class="flex items-center h-5">
-                      <input id="remember" aria-describedby="remember" type="checkbox"
-                             class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-purple-800 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-purple-600 dark:ring-offset-gray-800"
-                             required="true  ">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-purple-800 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-purple-600 dark:ring-offset-gray-800"
+                        required="true  "
+                      />
                     </div>
                     <div class="ml-3 text-sm">
-                      <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                      <label
+                        for="remember"
+                        class="text-gray-500 dark:text-gray-300"
+                        >Remember me</label
+                      >
                     </div>
                   </div>
-                  <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
-                    password?</a>
+                  <a
+                    href="#"
+                    class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >Forgot password?</a
+                  >
                 </div>
-                <button type="submit"
-                        class=" w-full focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 disabled:bg-purple-400 disabled:cursor-not-allowed"
-                        [disabled]="signinForm.status==='INVALID'">Sign in
+                <button
+                  type="submit"
+                  class=" w-full focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 disabled:bg-purple-400 disabled:cursor-not-allowed"
+                  [disabled]="signinForm.status === 'INVALID'"
+                >
+                  Sign in
                 </button>
 
                 <p class="text-sm  font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet? <a [routerLink]="['','signup']"
-                                                class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
-                  up</a>
+                  Don’t have an account yet?
+                  <a
+                    [routerLink]="['', 'signup']"
+                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >Sign up</a
+                  >
                 </p>
               </form>
             </div>
@@ -71,7 +124,7 @@ import {ToastService} from "../toast.service";
   styles: [``],
 })
 export class SigninComponent {
-  toast= inject(ToastService)
+  toast = inject(ToastService);
   signinForm = inject(FormBuilder).group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
@@ -103,7 +156,7 @@ export class SigninComponent {
               const user = jwtDecode(token) as IUser;
               console.log(user);
               this.stateService.user.set({ ...user, token });
-              this.toast.showNotification(`welcome,${user.fullname}`)
+              this.toast.showNotification(`welcome,${user.fullname}`);
               this.router.navigate(['', 'groups']);
             } catch (error) {
               console.error('Error decoding JWT:', error);
--- frontend/src/app/user/signup.component.ts
+++ frontend/src/app/user/signup.component.ts
@@ -8,38 +8,115 @@ import { UserService } from './user.service';
   template: `
     <div>
       <section class="bg-gray-50 dark:bg-gray-900">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            <img class=" h-8 mr-2" src="../assets/images/logo.png" alt="logo">
+        <div
+          class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+        >
+          <a
+            href="#"
+            class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img class=" h-8 mr-2" src="../assets/images/logo.png" alt="logo" />
             CoinSplit
           </a>
-          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div
+            class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+          >
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <h1
+                class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+              >
                 Create new Account
               </h1>
-              <form class="space-y-4 md:space-y-6" [formGroup]="signupForm" (ngSubmit)="signUp()">
+              <form
+                class="space-y-4 md:space-y-6"
+                [formGroup]="signupForm"
+                (ngSubmit)="signUp()"
+              >
                 <div>
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your full name</label>
-                  <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="true" formControlName="fullname">
+                  <label
+                    for="fullname"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Your full name</label
+                  >
+                  <input
+                    type="text"
+                    name="fullname"
+                    id="fullname"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Full Name"
+                    required="true"
+                    formControlName="fullname"
+                  />
                 </div>
                 <div>
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="true" formControlName="email">
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Your email</label
+                  >
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    autocomplete="username"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required="true"
+                    formControlName="email"
+                  />
                 </div>
                 <div>
-                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="true" formControlName="password">
+                  <label
+                    for="password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Password</label
+                  >
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autocomplete="new-password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required="true"
+                    formControlName="password"
+                  />
                 </div>
                 <div>
-                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Re-enter password</label>
-                  <input type="password" name="password"  placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="true" formControlName="rePassword" (keyup)="comparePassword()">
-                  <p class="text-xs text-red-500" *ngIf="!passwordMatch">*password does not match</p>
+                  <label
+                    for="password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Re-enter password</label
+                  >
+                  <input
+                    type="password"
+                    name="password"
+                    autocomplete="new-password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required="true"
+                    formControlName="rePassword"
+                    (keyup)="comparePassword()"
+                  />
+                  <p class="text-xs text-red-500" *ngIf="!passwordMatch">
+                    *password does not match
+                  </p>
                 </div>
 
-                <button type="submit" class=" w-full focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 disabled:bg-purple-400 disabled:cursor-not-allowed" [disabled]="signupForm.status==='INVALID' || !passwordMatch " >Sign up</button>
+                <button
+                  type="submit"
+                  class=" w-full focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 disabled:bg-purple-400 disabled:cursor-not-allowed"
+                  [disabled]="signupForm.status === 'INVALID' || !passwordMatch"
+                >
+                  Sign up
+                </button>
                 <p class="text-sm  font-light text-gray-500 dark:text-gray-400">
-                  Already have an account? <a [routerLink]="['','signin']" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</a>
+                  Already have an account?
+                  <a
+                    [routerLink]="['', 'signin']"
+                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >Sign in</a
+                  >
                 </p>
               </form>
             </div>
@@ -48,16 +125,29 @@ import { UserService } from './user.service';
       </section>
     </div>
   `,
-  styles: [``,
-  ],
+  styles: [``],
 })
 export class SignupComponent {
-  passwordMatch= true
+  passwordMatch = true;
   signupForm = inject(FormBuilder).group({
     fullname: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
-    password:['',[Validators.required,Validators.min(8),Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).+$')]],
-    rePassword:['',[Validators.required,Validators.min(8),Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).+$')]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.min(8),
+        Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).+$'),
+      ],
+    ],
+    rePassword: [
+      '',
+      [
+        Validators.required,
+        Validators.min(8),
+        Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).+$'),
+      ],
+    ],
   });
 
   private userService = inject(UserService);
@@ -90,9 +180,10 @@ export class SignupComponent {
       });
     }
   }
-  comparePassword(){
-    this.passwordMatch = this.signupForm.value.password === this.signupForm.value.rePassword;
-    console.log(this.signupForm.status)
-    console.log(this.passwordMatch)
+  comparePassword() {
+    this.passwordMatch =
+      this.signupForm.value.password === this.signupForm.value.rePassword;
+    console.log(this.signupForm.status);
+    console.log(this.passwordMatch);
   }
 }
